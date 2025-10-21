17e19a705183ec5de5963954f6a99c2c3a55c743
Mikyle Reyes <reyesmikyle17@gmail.com>
2025-10-20 18:47:51 -0700

    Create a component for the students page monthly meetings table

---
+++ src/components/monthly-meetings/StudentPageMonthlyMeetingsTable.tsx
@@ -0,0 +1,9 @@
+const StudentPageMonthlyMeetingsTable = () => {
+  return (
+    <div>
+      <p>Student Page Monthly Meetings Table</p>
+    </div>
+  )
+}
+
+export default StudentPageMonthlyMeetingsTable
