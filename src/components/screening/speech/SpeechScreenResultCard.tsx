e79e1a12a27d71541981135b9b9361cbce54053c
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-03-19 01:35:17 +0800

    Init speech screening result card component

---
+++ src/components/screening/speech/SpeechScreenResultCard.tsx
@@ -0,0 +1,16 @@
+import { UseFormReturn } from 'react-hook-form'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+import { Checkbox } from '@/components/ui/checkbox'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { SpeechScreeningFormValues } from '@/types/screening-form'
+
+interface SpeechScreenResultCardProps {
+  form: UseFormReturn<SpeechScreeningFormValues>
+}
