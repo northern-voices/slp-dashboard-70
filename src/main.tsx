05c0818deea1f714bde5cb5e994395cb8574dbcd
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-04-05 11:52:08 +0800

    ADjsut main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+import './instrument'
+import * as Sentry from '@sentry/react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <Sentry.ErrorBoundary fallback={<p>Something went wrong</p>} showDialog>
+    <App />
+  </Sentry.ErrorBoundary>
+)
