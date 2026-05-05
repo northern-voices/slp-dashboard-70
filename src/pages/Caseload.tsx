00181174fcb5bf2f0803f8ccebfc210aa0fec887
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-05-05 16:38:55 -0700

    Add initial code for caseload page

--- src/pages/Caseload.tsx
+++ src/pages/Caseload.tsx
@@ -1,1 +1,12 @@
 import React from 'react'
+
+const Caseload = () => {
+  return (
+    <div className='p-6'>
+      <h1 className='text-2xl font-semibold text-gray-900'>Caseload</h1>
+      <p className='mt-2 text-gray-500'>Caseload Page</p>
+    </div>
+  )
+}
+
+export default Caseload
