fe9f86409b4a95432ca296bc756fd522c73b530f
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-05-06 00:23:43 -0700

    Add loading state

--- src/components/caseload/CaseloadTable.tsx
+++ src/components/caseload/CaseloadTable.tsx
@@ -148,4 +148,16 @@ const CaseloadTable = ({ students, isLoading, schoolId }: CaseloadTableProps) =>
 
     return <ChevronDown className='w-4 h-4' />
   }
+
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center py-8'>
+        <div className='text-center'>
+          <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600 mx-auto mb-4'>
+            <p className='text-gray-600 text-sm'>Loading caseload...</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
