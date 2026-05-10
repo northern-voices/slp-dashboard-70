a5790433c95a0cdf40f11bf08e11c4ef94be36c7
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-05-10 10:58:35 -0700

    Add new screening options constants

---
+++ src/constants/screeningOptions.ts
@@ -0,0 +1,27 @@
+export const RESULT_OPTIONS = [
+  { value: 'no_errors', label: 'No Errors' },
+  { value: 'age_appropriate', label: 'Age Appropriate' },
+  { value: 'monitor', label: 'Monitor' },
+  { value: 'mild', label: 'Mild' },
+  { value: 'moderate', label: 'Moderate' },
+  { value: 'severe', label: 'Severe' },
+  { value: 'profound', label: 'Profound' },
+  { value: 'complex_needs', label: 'Complex Needs' },
+  { value: 'unable_to_screen', label: 'Non-Compliant' },
+  { value: 'absent', label: 'Absent' },
+  { value: 'non_registered_no_consent', label: 'No Consent' },
+]
+
+export const PROGRAM_OPTIONS = [
+  { value: 'qualified', label: 'Qualifies' },
+  { value: 'not_in_program', label: 'Not In Program' },
+  { value: 'sub', label: 'Sub' },
+  { value: 'no_consent', label: 'Qualifies - No Consent' },
+]
+
+export const SERVICE_STATUS_OPTIONS = [
+  { value: 'none', label: 'None' },
+  { value: 'paused', label: 'Pause/Away' },
+  { value: 'graduated', label: 'Graduated' },
+  { value: 'transferred', label: 'Transferred' },
+]
