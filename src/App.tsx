99ab4527dad003980ef23fa4c373f09f8779e35c
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-05-17 10:50:59 -0700

    Add goal sheets page on the app routing

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,7 @@ import MonthlyMeetings from './pages/monthly-meetings/MonthlyMeetings'
 import CreateMonthlyMeeting from './pages/monthly-meetings/CreateMonthlyMeeting'
 import EditMonthlyMeeting from './pages/monthly-meetings/EditMonthlyMeeting'
 import Caseload from './pages/Caseload'
+import GoalSheets from './pages/GoalSheets'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -463,6 +464,24 @@ const App = () => (
                       </SchoolRouter>
                     }
                   />
+
+                  <Route
+                    path='/speech-screening-reports/goal-sheets'
+                    element={
+                      <SchoolRouter>
+                        <GoalSheets />
+                      </SchoolRouter>
+                    }
+                  />
+                  <Route
+                    path='/school/:schoolId/speech-screening-reports/goal-sheets'
+                    element={
+                      <SchoolRouter>
+                        <GoalSheets />
+                      </SchoolRouter>
+                    }
+                  />
+
                   <Route
                     path='/school/:schoolId/speech-screening-reports/school-wide-hearing'
                     element={
