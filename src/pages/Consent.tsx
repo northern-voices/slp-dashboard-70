7de20c820db531a31c839cfc4c09ae8452d3caab
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-07-14 16:19:48 -0700

    Add initial consent code

---
+++ src/pages/Consent.tsx
@@ -0,0 +1,18 @@
+const Consent = () => {
+  return (
+    <div className='space-y-6'>
+      <div className='flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between'>
+        <div>
+          <h1 className='text-2xl font-semibold text-gray-900 lg:text-3xl'>Consent</h1>
+          <p className='text-gray-600'>Manage student consent forms</p>
+        </div>
+      </div>
+
+      <div className='rounded-lg border border-dashed p-8 text-center text-gray-500'>
+        Coming soon
+      </div>
+    </div>
+  )
+}
+
+export default Consent
