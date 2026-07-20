16264f447d40421c0b021812283912db4674acef
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-07-20 08:37:43 -0400

    Init admin tab content

---
+++ src/components/management/AdminTabContent.tsx
@@ -0,0 +1,3 @@
+import { Shield, Activity } from 'lucide-react'
+import AdminSecuritySettings from '@/components/admin/AdminSecuritySettings'
+import AdminActivityLogs from '@/components/admin/AdminActivityLogs'
