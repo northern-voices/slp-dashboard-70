019ab71687037e4ed0c45da117b10a7ce348b6b7
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-07-28 23:15:12 -0400

    Add minor bug

--- src/hooks/useDefaultReportPassword.ts
+++ src/hooks/useDefaultReportPassword.ts
@@ -3,5 +3,5 @@ import { useOrganization } from '@/contexts/OrganizationContext'
 export const useDefaultReportPassword = () => {
   const { userProfile, currentOrganization } = useOrganization()
 
-  return userProfile?.default_report_password || currentOrganization.default_report_password || ''
+  return userProfile?.default_report_password || currentOrganization?.default_report_password || ''
 }
