f560e84d74e4c7db10d5b8838d1946abee8307d4
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-07-28 22:34:53 -0400

    Add types and imports

---
+++ src/components/shared/ReportPasswordInput.tsx
@@ -0,0 +1,12 @@
+import { Eye, EyeOff, Lock } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+interface ReportPasswordInputProps {
+  password: string
+  onChange: (password: string) => void
+}
+
+const ReportPasswordInput = ({}) => {}
+
+export default ReportPasswordInput
