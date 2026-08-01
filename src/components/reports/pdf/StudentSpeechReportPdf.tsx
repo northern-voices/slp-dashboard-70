c8760643eb8edcfa82ae064ac0e329242d56d8d5
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-07-31 17:00:37 -0700

    Init student speech report pdf

---
+++ src/components/reports/pdf/StudentSpeechReportPdf.tsx
@@ -0,0 +1,3 @@
+import { Document, Page, View, Text, Image, StyleSheet } from '@react-pdf/renderer'
+import { REPORT_RESULTS_TEXT } from '@/constants/reportResultsText'
+import { DEVELOPMENTAL_CHART } from '@/constants/developmentalSpeechChart'
