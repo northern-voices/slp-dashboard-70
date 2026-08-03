b30e7b862e0d510096bfd7054bef31d68d5b2f34
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-03 11:20:24 -0700

    Init speech progess report  pdf

---
+++ src/components/reports/pdf/SpeechProgressReportPdf.tsx
@@ -0,0 +1,60 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq6R9WXh0o5C6MLk.ttf',
+      fontWeight: 400,
+      fontStyle: 'italic',
+    },
+  ],
+})
+
+interface ProgressTableError {
+  sound: string
+  pattern: string
+  example: string
+  initial_screen: string
+  progress_screen: string
+  summary: string
+}
+
+interface SpeechProgressReportData {
+  context: {
+    student_name: string
+    grade: string
+    school: string
+    initial_screen_date: string
+    progress_screen_date: string
+    primary_table_errors: ProgressTableError[]
+    progress_notes: string
+  }
+}
