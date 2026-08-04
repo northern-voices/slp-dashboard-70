1337cb28681055c916518e7e044ec83bae50e44f
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-04 11:59:13 -0700

    Add no errors speech report pdf

---
+++ src/components/reports/pdf/NoErrorsSpeechReportPdf.tsx
@@ -0,0 +1,48 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+import { DEVELOPMENTAL_CHART } from '@/constants/developmentalSpeechChart'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq6R9WXh0o5C6MLk.ttf',
+      fontWeight: 400,
+      fontStyle: 'italic',
+    },
+  ],
+})
+
+interface NoErrorsReportData {
+  context: {
+    student_name: string
+    date_of_screening: string
+    grade: string
+  }
+}
