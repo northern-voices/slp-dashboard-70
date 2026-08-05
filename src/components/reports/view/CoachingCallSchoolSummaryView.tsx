d235255d1243c5d3073345352b35616916a8c86c
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 15:28:31 -0700

    Add types

---
+++ src/components/reports/view/CoachingCallSchoolSummaryView.tsx
@@ -0,0 +1,17 @@
+interface CoachingCallSchoolSummaryData {
+  context: {
+    meeting_title: string
+    facilitator_name: string
+    school: string
+    meeting_date: string
+    attendees: string[]
+    has_topics: boolean
+    topics: string
+    has_visit_purpose: boolean
+    school_visit_purpose: string
+    has_additional_notes: boolean
+    additional_notes: string
+    has_action_plan: boolean
+    action_plan: string
+  }
+}
