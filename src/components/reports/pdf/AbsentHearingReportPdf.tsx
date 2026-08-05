11f19b0060b52954b5af6799fae4793de1e07f8f
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 13:18:50 -0700

    init absent hearing report

---
+++ src/components/reports/pdf/AbsentHearingReportPdf.tsx
@@ -0,0 +1,53 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Caveat',
+  src: 'https://fonts.gstatic.com/s/caveat/v18/WnznHAc5bAfYB2QRah7pcpNvOx-pjfJ9SIKih_a2wg.ttf',
+})
