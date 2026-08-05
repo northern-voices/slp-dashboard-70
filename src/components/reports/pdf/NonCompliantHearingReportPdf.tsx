e9ef3e5c0226363afd0081dbc84e2d821f9cdf6d
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 13:25:23 -0700

    Add non compliant hearing report pdf

---
+++ src/components/reports/pdf/NonCompliantHearingReportPdf.tsx
@@ -0,0 +1,57 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Caveat',
+  src: 'https://fonts.gstatic.com/s/caveat/v18/WnznHAc5bAfYB2QRah7pcpNvOx-pjfJ9SIKih_a2wg.ttf',
+})
+
+interface HearingReportData {
+  context: {
+    student_name: string
+    date_of_screening: string
+    grade: string
+  }
+}
+
+const SIGNS_LIST = [
+  'Trouble understanding or following instructions',
+  'Unclear speech',
+  'Not responding to name or environmental sounds',
+  'Frequent requests for repetition (e.g., saying, "What?" or "Huh?")',
+  'Complaints of ear pain, ringing, or noises in the ear(s)',
+  'Turning up the volume on the TV or music devices',
+  'Speaking loudly or watching lips closely when listening to others',
+  'Challenges with reading or academic progress',
+]
