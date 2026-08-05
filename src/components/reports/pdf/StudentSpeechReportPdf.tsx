d65659e7a8ba8be4e9a0bbd41eb14f45042aed7c
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 10:54:46 -0700

    Wire remaining speech screening result variants into PDF dispatcher

--- src/components/reports/pdf/StudentSpeechReportPdf.tsx
+++ src/components/reports/pdf/StudentSpeechReportPdf.tsx
@@ -1,5 +1,9 @@
 import GenericSpeechScreeningReportPdf from './GenericSpeechScreeningReportPdf'
 import NoErrorsSpeechReportPdf from './NoErrorsSpeechReportPdf'
+import NonCompliantSpeechReportPdf from './NonCompliantSpeechReportPdf'
+import AbsentSpeechReportPdf from './AbsentSpeechReportPdf'
+import MildProfoundNoQualifiedSubReportPdf from './MildProfoundNoQualifiedSubReportPdf'
+import MildProfoundQualifiedSubReportPdf from './MildProfoundQualifiedSubReportPdf'
 
 interface StudentSpeechReportData {
   template?: { name?: string }
@@ -16,6 +20,14 @@ const StudentSpeechReportPdf = ({ data }: { data: StudentSpeechReportData }) => 
   switch (data.template?.name) {
     case 'No Errors':
       return <NoErrorsSpeechReportPdf data={data as never} />
+    case 'Non Compliant':
+      return <NonCompliantSpeechReportPdf data={data as never} />
+    case 'Absent':
+      return <AbsentSpeechReportPdf data={data as never} />
+    case 'Mild Profound No Qualified Sub':
+      return <MildProfoundNoQualifiedSubReportPdf data={data as never} />
+    case 'Mild Profound Qualified Sub':
+      return <MildProfoundQualifiedSubReportPdf data={data as never} />
     default:
       return <GenericSpeechScreeningReportPdf data={data as never} />
   }
