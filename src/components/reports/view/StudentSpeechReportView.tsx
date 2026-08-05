6c974b95cadf835ca5e4610d5c80e1c20b1ac7fd
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 10:54:53 -0700

    Wire remaining speech screening result variants into view dispatcher

--- src/components/reports/view/StudentSpeechReportView.tsx
+++ src/components/reports/view/StudentSpeechReportView.tsx
@@ -1,5 +1,10 @@
 import GenericSpeechScreeningReportView from './GenericSpeechScreeningReportView'
 import NoErrorsSpeechReportView from './NoErrorsSpeechReportView'
+import NonCompliantSpeechReportView from './NonCompliantSpeechReportView'
+import AbsentSpeechReportView from './AbsentSpeechReportView'
+import MildProfoundNoQualifiedSubReportView from './MildProfoundNoQualifiedSubReportView'
+import MildProfoundQualifiedSubReportView from './MildProfoundQualifiedSubReportView'
+import PosterOnlySpeechReportView from './PosterOnlySpeechReportView'
 
 interface StudentSpeechReportData {
   metadata?: { file_name?: string }
@@ -24,6 +29,17 @@ const StudentSpeechReportView = ({ data }: { data: StudentSpeechReportData }) =>
   switch (data.template?.name) {
     case 'No Errors':
       return <NoErrorsSpeechReportView data={data as never} />
+    case 'Non Compliant':
+      return <NonCompliantSpeechReportView data={data as never} />
+    case 'Absent':
+      return <AbsentSpeechReportView data={data as never} />
+    case 'Mild Profound No Qualified Sub':
+      return <MildProfoundNoQualifiedSubReportView data={data as never} />
+    case 'Mild Profound Qualified Sub':
+      return <MildProfoundQualifiedSubReportView data={data as never} />
+    case 'Complex Needs':
+    case 'Non Registered No Consent':
+      return <PosterOnlySpeechReportView />
     default:
       return <GenericSpeechScreeningReportView data={data as never} />
   }
