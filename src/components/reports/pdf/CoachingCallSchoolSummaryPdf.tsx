6b71c5e3a6aa6a1687f79311b48e4e9a0243442e
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 15:26:54 -0700

    Init coaching call school summary pdf

---
+++ src/components/reports/pdf/CoachingCallSchoolSummaryPdf.tsx
@@ -0,0 +1,59 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq6R9WXh0o5C6MLk.ttf',
+      fontWeight: 400,
+      fontStyle: 'italic',
+    },
+  ],
+})
+
+interface CoachingCallSchoolSummaryData {
+  context: {
+    meeting_title: string
+    facilitator_name: string
+    school: string
+    meeting_date: string
+    attendees: string[]
+    has_topics: boolean
+    topics: string
+    has_visit_purpose: boolean
+    school_visit_purpose: string
+    has_additional_notes: boolean
+    additional_notes: string
+    has_action_plan: boolean
+    action_plan: string
+  }
+}
+
+const BANNER_BG = '#5b7a8b'
