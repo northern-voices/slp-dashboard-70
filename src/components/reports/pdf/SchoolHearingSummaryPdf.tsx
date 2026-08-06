a1c835c3667a487e828599deafb0f65042759826
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-05 17:02:22 -0700

    init and add types

---
+++ src/components/reports/pdf/SchoolHearingSummaryPdf.tsx
@@ -0,0 +1,61 @@
+import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Gotu',
+  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
+})
+
+Font.register({
+  family: 'Nunito',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+Font.register({
+  family: 'Montserrat',
+  fonts: [
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
+      fontWeight: 400,
+    },
+    {
+      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
+      fontWeight: 700,
+    },
+  ],
+})
+
+interface SummaryStudent {
+  name: string
+  grade: string
+}
+
+interface SchoolHearingSummaryData {
+  context: {
+    screening_date: string
+    referred: boolean
+    referred_students: SummaryStudent[]
+    absent?: boolean
+    absent_students?: SummaryStudent[]
+  }
+}
+
+interface TableBlock {
+  heading: string
+  columns: string[]
+  rows: string[][]
+}
+
+interface PageSegment {
+  heading: string
+  columns: string[]
+  rows: string[][]
+}
