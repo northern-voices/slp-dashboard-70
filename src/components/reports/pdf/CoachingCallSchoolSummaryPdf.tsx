6a175f33ed2bd81b609f36da9115819275c631fe
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-07 20:03:41 -0700

    Cleanup for reusability

--- src/components/reports/pdf/CoachingCallSchoolSummaryPdf.tsx
+++ src/components/reports/pdf/CoachingCallSchoolSummaryPdf.tsx
@@ -1,44 +1,6 @@
 import { Document, Page, View, Text, Image, StyleSheet, Font } from '@react-pdf/renderer'
 import { ReportBanner, ReportFooter } from './shared/reportBannerChrome'
 
-Font.register({
-  family: 'Gotu',
-  src: 'https://fonts.gstatic.com/s/gotu/v18/o-0FIpksx3QOpHoBjqp56hQ.ttf',
-})
-
-Font.register({
-  family: 'Nunito',
-  fonts: [
-    {
-      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3iqzdXWg.ttf',
-      fontWeight: 400,
-    },
-    {
-      src: 'https://fonts.gstatic.com/s/nunito/v32/XRXI3I6Li01BKofiOc5wtlZ2di8HDFwmdTQ3iqzdXWg.ttf',
-      fontWeight: 700,
-    },
-  ],
-})
-
-Font.register({
-  family: 'Montserrat',
-  fonts: [
-    {
-      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Hw5aX9-obK4.ttf',
-      fontWeight: 400,
-    },
-    {
-      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCuM73w5aX9-obK4.ttf',
-      fontWeight: 700,
-    },
-    {
-      src: 'https://fonts.gstatic.com/s/montserrat/v31/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq6R9WXh0o5C6MLk.ttf',
-      fontWeight: 400,
-      fontStyle: 'italic',
-    },
-  ],
-})
-
 interface CoachingCallSchoolSummaryData {
   context: {
     meeting_title: string
@@ -69,32 +31,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Nunito',
     color: '#374151',
   },
-  banner: {
-    backgroundColor: BANNER_BG,
-    paddingVertical: 16,
-    paddingHorizontal: 48,
-    marginBottom: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  bannerTitle: { fontFamily: 'Gotu', fontSize: 30, color: '#ffffff', letterSpacing: 0.5 },
-  bannerBrand: { flexDirection: 'row', alignItems: 'center' },
-  bannerLogo: { width: 26, height: 26, borderRadius: 4, marginRight: 8 },
-  bannerBrandText: {
-    fontSize: 9,
-    fontFamily: 'Nunito',
-    fontWeight: 700,
-    letterSpacing: 1,
-    color: '#ffffff',
-  },
-  bannerBrandSub: {
-    fontSize: 6,
-    fontFamily: 'Montserrat',
-    letterSpacing: 2,
-    color: '#e5eaec',
-    marginTop: 2,
-  },
   body: { paddingHorizontal: 48 },
 
   infoLine: { marginBottom: 8, fontSize: 11 },
@@ -109,23 +45,6 @@ const styles = StyleSheet.create({
     marginTop: 8,
   },
   sectionText: { fontSize: 10, color: '#374151', lineHeight: 1.4, marginBottom: 8 },
-
-  footer: {
-    position: 'absolute',
-    bottom: 32,
-    left: 48,
-    right: 48,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: '#e5e7eb',
-    paddingTop: 8,
-    fontSize: 8,
-    color: '#6b7280',
-  },
-  footerLogo: { width: 12, height: 12, marginRight: 4 },
-  footerPage: { flexDirection: 'row', alignItems: 'center' },
 })
 
 const CoachingCallSchoolSummaryPdf = ({ data }: { data: CoachingCallSchoolSummaryData }) => {
