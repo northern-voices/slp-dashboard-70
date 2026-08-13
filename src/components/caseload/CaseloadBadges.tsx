ce59c89a6c991aeba4979852c835518579a639af
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-13 10:48:38 -0700

    Add imports

---
+++ src/components/caseload/CaseloadBadges.tsx
@@ -0,0 +1,4 @@
+import { Badge } from '@/components/ui/badge'
+import { FileCheck, FileX } from 'lucide-react'
+import { SCREENING_RESULTS } from '@/constants/screeningResults'
+import { ProgramStatus, ServiceStatus } from '@/types/database'
