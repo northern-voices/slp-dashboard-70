ad0a3f672b34061144f032667ffda85043f3d2dd
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-21 15:41:20 -0700

    Add and display new posters for the no conset, etc

--- src/components/reports/view/PosterOnlySpeechReportView.tsx
+++ src/components/reports/view/PosterOnlySpeechReportView.tsx
@@ -2,7 +2,7 @@ const PosterOnlySpeechReportView = () => (
   <div className='space-y-6 print:space-y-0'>
     <section className='bg-white shadow-sm w-full overflow-hidden'>
       <img
-        src='/teachspeech-app-poster.jpg'
+        src='/No-Consent_Non-Registered_Complex-Needs.jpg'
         alt='Free access to the NVSS TeachSpeech app'
         className='w-full h-auto block'
       />
