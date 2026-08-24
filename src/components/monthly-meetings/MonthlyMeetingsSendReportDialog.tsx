6e526b953406640e59813379ae8eada2fffa3cee
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-08-24 10:54:44 -0700

    Fix bug where user's email and default report password disappears after sending a report

--- src/components/monthly-meetings/MonthlyMeetingsSendReportDialog.tsx
+++ src/components/monthly-meetings/MonthlyMeetingsSendReportDialog.tsx
@@ -51,8 +51,8 @@ const MonthlyMeetingsSendReportDialog = ({
   const emailHistory = useEmailSuggestions(user?.id, currentSchool?.id)
 
   const handleClose = () => {
-    setEmails([])
-    setPassword('')
+    setRecipientEmails(user?.email ? [user.email] : [])
+    setPassword(defaultReportPassword)
     onClose()
   }
 
