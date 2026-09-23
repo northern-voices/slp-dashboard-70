d2af4815e9d669525feafea70a650b21caef5656
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-09-23 16:14:04 -0700

    Flip regular screening reports to sound-errors poster wording

--- src/components/reports/view/AbsentSpeechReportView.tsx
+++ src/components/reports/view/AbsentSpeechReportView.tsx
@@ -97,7 +97,7 @@ const AbsentSpeechReportView = ({ data }: { data: AbsentReportData }) => {
 
       <section className='bg-white shadow-sm w-full overflow-hidden'>
         <img
-          src='/teachspeech-app-poster.jpg'
+          src='/teachspeech-app-poster-sound-errors.jpg'
           alt='Free access to the NVSS TeachSpeech app'
           className='w-full h-auto block'
         />
