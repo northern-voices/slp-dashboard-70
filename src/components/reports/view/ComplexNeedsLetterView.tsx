155c5d30e81356ecd1e6fe164c78675dc66ef508
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-09-23 15:54:53 -0700

    Always use sound-errors poster wording for Complex Needs letters

--- src/components/reports/view/ComplexNeedsLetterView.tsx
+++ src/components/reports/view/ComplexNeedsLetterView.tsx
@@ -1,16 +1,8 @@
 import { ReportHeader, ReportFooter } from './shared/ReportSimpleChrome'
 
-interface ProcessedError {
-  sound: string
-  pattern: string
-  example: string
-  targetSound: string
-}
-
 interface ComplexNeedsLetterData {
   context: {
     student_name: string
-    errors?: ProcessedError[]
   }
 }
 
@@ -62,11 +54,7 @@ const ComplexNeedsLetterView = ({ data }: { data: ComplexNeedsLetterData }) => {
 
       <section className='bg-white shadow-sm w-full overflow-hidden'>
         <img
-          src={
-            context.errors?.length > 0
-              ? '/No-Consent_Non-Registered_Complex-Needs-sound-errors.jpg'
-              : '/No-Consent_Non-Registered_Complex-Needs.jpg'
-          }
+          src='/No-Consent_Non-Registered_Complex-Needs-sound-errors.jpg'
           alt='Free access to the NVSS TeachSpeech app'
           className='w-full h-auto block'
         />
