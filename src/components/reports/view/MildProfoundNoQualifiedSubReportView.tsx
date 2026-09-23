dce0d457a81d609790da8b13515b2a265de1c6a9
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-09-23 15:23:01 -0700

    Show sound-errors TeachSpeech poster variant when errors exist

--- src/components/reports/view/MildProfoundNoQualifiedSubReportView.tsx
+++ src/components/reports/view/MildProfoundNoQualifiedSubReportView.tsx
@@ -175,7 +175,11 @@ const MildProfoundNoQualifiedSubReportView = ({
 
       <section className='bg-white shadow-sm w-full overflow-hidden'>
         <img
-          src='/teachspeech-app-poster.jpg'
+          src={
+            context.errors?.length > 0
+              ? '/teachspeech-app-poster-sound-errors.jpg'
+              : '/teachspeech-app-poster.jpg'
+          }
           alt='Free access to the NVSS TeachSpeech app'
           className='w-full h-auto block'
         />
