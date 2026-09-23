af84d80369e977040a925b9dd915fa1027401d42
Mikyle Reyes <reyesmikyle17@gmail.com>
2026-09-23 15:54:31 -0700

    Always use no-errors TeachSpeech poster wording for regular screening reports

--- src/components/reports/view/GenericSpeechScreeningReportView.tsx
+++ src/components/reports/view/GenericSpeechScreeningReportView.tsx
@@ -194,11 +194,7 @@ const GenericSpeechScreeningReportView = ({ data }: { data: StudentSpeechReportD
 
       <section className='bg-white shadow-sm w-full overflow-hidden'>
         <img
-          src={
-            context.errors?.length > 0
-              ? '/teachspeech-app-poster-sound-errors.jpg'
-              : '/teachspeech-app-poster.jpg'
-          }
+          src='/teachspeech-app-poster.jpg'
           alt='Free access to the NVSS TeachSpeech app'
           className='w-full h-auto block'
         />
